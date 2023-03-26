0d4676a5658075761104a3e15230b03919058c33
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2023-03-26 02:54:57 -0300

    Se finaliza jornada de estudio preparando el ejerciocio 07

---
+++ Tema 1/repaso-1/08-buscaMatriz/BuscaMatriz.cpp
@@ -0,0 +1,14 @@
+/**
+    1. Hacer una función que reciba como parámetros una matriz de enteros de 10x10
+    y un número entero (entre 0 y 9), y devuelva el máximo de la fila indicada por el
+    segundo parámetro.
+**/
+
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+
+  return 0;
+}
