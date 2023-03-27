d870aa064cde159550016a4d24cab7f66ed75671
Samuel Medina <samueltomasmedina_1999@hotmail.com>
2023-03-27 01:49:10 -0300

    ejercicio de matriz resuelto

--- Tema 1/repaso-1/08-buscaMatriz/BuscaMatriz.cpp
+++ Tema 1/repaso-1/08-buscaMatriz/BuscaMatriz.cpp
@@ -7,9 +7,43 @@
 #include <iostream>
 using namespace std;
 
-int main(void){
+int buscaMatriz(int [10][10], int);
 
+int main(void){
+    const int TAM = 10;
+    int num = 7;
+
+    int m[TAM][TAM] = {
+        {1,2,3,4,5,66,7,8,9,19},
+         {1,2,3,4,55,6,7,8,9,19},
+         {1,2,3,4,5,6,7,8,9,19},
+         {1,2,3,4555,5,46,7,8,9,19},
+         {1,2,37,4,5,6,7,8,96,19},
+         {1,2,3,4,5,56,7,8,95,19},
+         {1,2,3,42,5,6,7,8,9,19},
+         {1,2,3,4,5,6,777,8,9,19},
+         {1,2,3,4,5,64,7,8,9,19},
+         {1,2,3,4,5,6,7,8,79,19}
+    };
+
+    cout << buscaMatriz(m, num-1);
 
 
   return 0;
+}
+
+int buscaMatriz(int m[][10], int num)
+{
+    const int TAM = 10;
+    int numMayor = m[num][0];
+
+    for(int i = 1; i < TAM; i++)
+    {
+        if(m[num][i] > numMayor)
+        {
+            numMayor = m[num][i];
+        }
+    }
+
+    return numMayor;
 }
